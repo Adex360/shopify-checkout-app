717c174ac35358d4213b7940394ea6deab17da1e
haroon <haroonshahzad203@gmail.com>
2024-08-22 02:08:59 +0500

    changes in extension

--- extensions/city-list/src/config/index.js
+++ extensions/city-list/src/config/index.js
@@ -1,4 +1,4 @@
 export const API_URL =
-  "https://smart-checkout-new.ozeefy.com/api/v1/city-list/all/";
+  "https://smart-checkout-new.ozeefy.com/api/v1/city-list/all";
 // export const API_URL =
 //   "https://lease-base-sterling-latin.trycloudflare.com/api/v1/city-list/all/";
