2760c6adc773461efd3efc0e08682a121063dc0d
ahmad122519 <ahmadali122519@gmail.com>
2024-09-04 16:33:42 +0500

    discounts setup

--- web/frontend/pages/discount-types/index.jsx
+++ web/frontend/pages/discount-types/index.jsx
@@ -11,57 +11,42 @@ const DiscountTypes = () => {
         title="Choose your customization type "
         backAction={{
           content: "",
-          onAction: () => navigate("/payment"),
+          onAction: () => navigate("/discounts"),
         }}
       >
         <Layout>
           <Layout.Section>
             <InlineGrid gap="400" alignItems="start" columns={2}>
               <CustomActionCard
-                title="Advance Payment rules(Hide/Delete)"
-                description='The "Advance Payment Rules (Hide/Delete)" feature allows you to manage and enforce specific payment conditions during checkout. It enables the hiding or deletion of payment options based on predefined rules, ensuring that customers adhere to your advance payment requirements.'
+                title="Product Discount"
+                description="Boost sales and enhance customer satisfaction with our Product Discount feature. Apply targeted discounts to specific products, with flexible options for percentage or fixed amount reductions. Tailor promotions to make your best-selling items even more appealing and drive more conversions."
                 action={
                   <>
                     <Link
                       onClick={() => {
-                        navigate("");
+                        navigate("/discount-types/product/create");
                       }}
                     >
-                      Create Customization
+                      Create Discount
                     </Link>
                   </>
                 }
               />
 
               <CustomActionCard
-                title="Sort/Re-order"
-                description="Provides functionality to rearrange elements within the checkout process, allowing users to adjust the sequence of items or fields. This helps to optimize the order of operations and enhances the overall user experience by aligning the checkout flow with specific needs or preferences."
+                title="Order Discount"
+                description="Encourage larger purchases and reward loyal customers with our Order Discount feature. Offer percentage or fixed amount discounts on the total order value, with customizable rules to drive higher cart values and boost overall sales."
                 action={
                   <>
                     <>
                       <Link
                         onClick={() => {
-                          navigate("/payment-customization/re-order/create");
+                          navigate("/discount-types/order/create");
                         }}
                       >
-                        Create Customization
+                        Create Discount
                       </Link>
                     </>
-                  </>
-                }
-              />
-              <CustomActionCard
-                title="Translate/Re-name"
-                description="Allows users to modify the language and terminology used in the checkout process. This includes translating field labels, instructions, and other text elements into different languages or renaming them to better fit the brand's voice or specific regional preferences, ensuring clarity and consistency for all users."
-                action={
-                  <>
-                    <Link
-                      onClick={() => {
-                        navigate("/payment-customization/rename/create");
-                      }}
-                    >
-                      Create Customization
-                    </Link>
                   </>
                 }
               />
---
+++ web/frontend/pages/discount-types/order/[id].jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OrderDiscount = () => {
+  return <div>OrderDiscount</div>;
+};
+
+export default OrderDiscount;
